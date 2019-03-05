a15efa7467a6ebf9603fdc9541101991b690d9ed
bmolina-nyc <bruce.molina.81@gmail.com>
2019-03-05 10:11:57 -0500

    final assessment 1 update

--- Week5/Assessment 1/algorithm/algo.py
+++ Week5/Assessment 1/algorithm/algo.py
@@ -11,12 +11,15 @@ def sorting(MyList):
                 idx = MyList.index(el)
                 pop = MyList.pop(idx)
                 MyList.append(pop)
+                print(check)
+                print(MyList)
+                input()
             elif el != 0:
                 continue   
-        if MyList[-list_check:] == check:
-            return MyList
-        else:
-            continue
+            if MyList[-list_check:] == check:
+                return MyList
+            else:
+                continue
 
 # print(sorting([1, 0, 7, 2, 0, 3, 9, 0, 4]))
 # [1, 7, 2, 3, 9, 4, 0, 0, 0]
