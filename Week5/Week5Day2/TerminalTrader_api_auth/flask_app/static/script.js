6e8ef53615f88961ee95d276231601fdb9331be3
bmolina-nyc <bruce.molina.81@gmail.com>
2019-03-18 10:39:33 -0400

    html quiz week 7

--- Week5/Week5Day2/TerminalTrader_api_auth/flask_app/static/script.js
+++ Week5/Week5Day2/TerminalTrader_api_auth/flask_app/static/script.js
@@ -1,13 +1,24 @@
 window.apikey = null 
 
+// navbar routes 
+const navbar = document.getElementById('navbar')
+const homeNav = document.getElementById("homeNav")
+const tickerNav = document.getElementById("tickerNav")
+const depositNav = document.getElementById("depositNav")
+const buyNav = document.getElementById("buyNav")
+const sellNav = document.getElementById("sellNav")
+const tradesNav = document.getElementById("tradesNav")
+
+// all forms
 const loginform = document.getElementById('form-login')
 const stockform = document.getElementById('stock')
+const depositform = document.getElementById('div-deposit')
+const tradeform = document.getElementById('trades')
 
 const formbutton = document.getElementById('login')
 const username_input =document.getElementById('username')
 const password_input = document.getElementById('password')
 
-const navbar = document.getElementById('navbar')
 
 // ticker items
 const inputArea = document.getElementById('ticker')
@@ -68,12 +79,19 @@ function price_click(e){
 tickerbutton.addEventListener('click',(e)=> e.preventDefault())
 function ticker_click(){
   outputAreaTrades.innerHTML = ""
+
+  if (inputAreaTrades.value === ""){
+    outputAreaTrades.innerHTML += `<strong>Please enter a value to look up</strong>`
+    return null 
+  } 
+
   prom = fetch(`http://127.0.0.1:5000/api/${window.apikey}/trades/${inputAreaTrades.value}`)
   prom.then(data => data.json())
-  .then(json =>{
+  .then(json =>{ 
     if (Object.entries(json["trades"]).length === 0){
-      outputAreaTrades.innerHTML += `<strong>You have no trades at this time for ${inputAreaTrades}</strong>`
-      } else {
+      outputAreaTrades.innerHTML += `<strong>You have no trades at this time for ${inputAreaTrades.value}</strong>`
+      } 
+    else {
         Object.keys(json["trades"]).forEach(key => {
           let trade = json["trades"][key]
           outputAreaTrades.innerHTML +=  `<ol><strong>TICKER: ${trade["ticker"]} || PRICE: ${trade["price"]} || VOLUME: ${trade["volume"]}</strong></ol>`
@@ -86,6 +104,7 @@ function ticker_click(){
 
 tradebutton.addEventListener('click',(e)=> e.preventDefault())
 function trades_click(e){
+  inputAreaTrades.value = ""
   outputAreaTrades.innerHTML = ""
   prom = fetch(`http://127.0.0.1:5000/api/${window.apikey}/alltrades`)
   prom.then(data => data.json())
@@ -101,4 +120,63 @@ function trades_click(e){
     })
 }
 
+function deposit_click() {
+  let dep_prom = fetch(`/api/${window.apikey}/deposit`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({'amount': parseFloat(amount.value)})
+  })
+  dep_prom.then(response => response.json())
+    .then(json=>{
+      const dollars = json.balance.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+      output.innerHTML = `<b>Your account balance is now $${dollars}</b>`
+    });
+};
  
+
+
+homeNav.addEventListener('click',(e)=> e.preventDefault())
+tickerNav.addEventListener('click',(e)=> e.preventDefault())
+depositNav.addEventListener('click',(e)=> e.preventDefault())
+buyNav.addEventListener('click',(e)=> e.preventDefault())
+sellNav.addEventListener('click',(e)=> e.preventDefault())
+tradesNav.addEventListener('click',(e)=> e.preventDefault())
+
+function showHome(){
+  loginform.classList.remove('hide')
+  stockform.classList.add('hide')
+  depositform.classList.add('hide')
+  tradeform.classList.add('hide')
+}
+function showTicker(){
+  loginform.classList.add('hide')
+  stockform.classList.remove('hide')
+  depositform.classList.add('hide')
+  tradeform.classList.add('hide')
+}
+function showDeposit(){
+  loginform.classList.add('hide')
+  stockform.classList.add('hide')
+  depositform.classList.remove('hide')
+  tradeform.classList.add('hide') 
+}
+function showBuy(){
+  loginform.classList.add('hide')
+  stockform.classList.add('hide')
+  depositform.classList.add('hide')
+  tradeform.classList.add('hide') 
+}
+function showSell(){
+  loginform.classList.add('hide')
+  stockform.classList.add('hide')
+  depositform.classList.add('hide')
+  tradeform.classList.add('hide') 
+}
+function showTrades(){
+  loginform.classList.add('hide')
+  stockform.classList.add('hide')
+  depositform.classList.add('hide')
+  tradeform.classList.remove('hide') 
+}
